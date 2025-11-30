b7a728687cc0222215a8a7877bf4ad64d3886dad
HirenChaudhari <95466106+HirenChaudhari@users.noreply.github.com>
2025-11-30 12:23:38 +0530

    structs.go

--- challenges/structs.go
+++ challenges/structs.go
@@ -18,14 +18,21 @@ type Inventory struct {
 // NewInventory creates a new empty Inventory.
 func NewInventory() *Inventory {
 	// TODO: Initialize and return a new Inventory
-	return nil
+	
+	return &Inventory {
+		Items: make(map[string]*Item),
+	}
 }
 
 // AddItem adds a new item to the inventory.
 // If an item with the same ID already exists, return an error.
 func (inv *Inventory) AddItem(id, name string, price float64, stock int) error {
 	// TODO: Implement this method
-	return errors.New("not implemented")
+	if inv.Items[id] != nil {
+		return errors.New("Item with this ID already exists")
+	}
+	inv.Items[id] = &Item{id, name, price, stock}
+	return nil
 }
 
 // UpdateStock updates the stock of an existing item.
@@ -33,11 +40,23 @@ func (inv *Inventory) AddItem(id, name string, price float64, stock int) error {
 // If the new stock is negative, return an error.
 func (inv *Inventory) UpdateStock(id string, newStock int) error {
 	// TODO: Implement this method
-	return errors.New("not implemented")
+	item := inv.Items[id]
+	if item == nil {
+		return errors.New("ID does not exist")
+	}
+	if newStock < 0 {
+		return errors.New("Negative Stock")
+	}
+	item.Stock = newStock
+	return nil
 }
 
 // GetTotalValue calculates the total value of the inventory (sum of Price * Stock for all items).
 func (inv *Inventory) GetTotalValue() float64 {
 	// TODO: Implement this method
-	return 0.0
+	var total float64
+	for _, i := range inv.Items {
+		total += (i.Price * float64 (i.Stock)) 
+	}
+	return total
 }
